3ca5346ac4f102081c87c0f7159918fa044a1f51
Nahid30 <nahidshuvo30@gmail.com>
2022-02-17 03:32:36 +0600

    added three input in one function & call the funtion & it works

--- js/custom.js
+++ js/custom.js
@@ -1,27 +1,50 @@
 
-
+function getInput(inputId){
+    const inputFood = document.getElementById(inputId);
+    const inputFoodValue = inputFood.value;
+    const inputFoodAmount = parseFloat(inputFoodValue);
+    inputFood.value = '';
+    return inputFoodAmount;
+}
 
 
 document.getElementById('calculate-total').addEventListener('click',function(){
 
-    const inputRent = document.getElementById('rent-input');
-    const inputRentValue = inputRent.value;
-    const inputRentAmount = parseFloat(inputRentValue);
-    console.log(inputRentAmount);
+    
+       /*  const inputFood = document.getElementById('food-input');
+        const inputFoodValue = inputFood.value;
+        const inputFoodAmount = parseFloat(inputFoodValue);
+        console.log(inputFoodAmount); */
+     
+   
+        const inputFoodAmount =  getInput('food-input');
+    
+     
+        /* const inputRent = document.getElementById('rent-input');
+        const inputRentValue = inputRent.value;
+        const inputRentAmount = parseFloat(inputRentValue);
+        console.log(inputRentAmount); */
+    
+        const inputRentAmount = getInput('rent-input');
     
 
-    const inputFood = document.getElementById('food-input');
-    const inputFoodValue = inputFood.value;
-    const inputFoodAmount = parseFloat(inputFoodValue);
-    console.log(inputFoodAmount);
-
-
-    const inputCloth = document.getElementById('cloth-input');
-    const inputClothValue = inputCloth.value;
-    const inputClothAmount = parseFloat(inputClothValue);
-    console.log(inputClothAmount);
     
-})
+        /* const inputCloth = document.getElementById('cloth-input');
+        const inputClothValue = inputCloth.value;
+        const inputClothAmount = parseFloat(inputClothValue);
+        console.log(inputClothAmount);    */
+
+        const inputClothAmount = getInput('cloth-input');
+    
+
+    
+        let totalExpences = inputFoodAmount + inputRentAmount + inputClothAmount;
+        console.log(totalExpences);
+    
+
+   
+    
+});
 
 
    
