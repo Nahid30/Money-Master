6af382509337f4272fab0e25645e28fcf5995314
Nahid30 <nahidshuvo30@gmail.com>
2022-02-17 21:43:03 +0600

    assignment is ready to submit just clean the comments left

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,33 @@
 
 // function for calculate button and calculation 
 function getInput(inputId){
+    
     const inputField = document.getElementById(inputId);
     const inputValue = inputField.value;
     const inputAmount = parseFloat(inputValue);
-    inputField.value = '';
-    return inputAmount;
+    if(inputField.value == ''){
+        
+        document.getElementById(inputId + '-fillup').style.display='block';
+
+    
+    }
+    else if(inputField.value >= 0 ){
+        document.getElementById(inputId + '-error').style.display='none';
+        document.getElementById(inputId + '-fillup').style.display='none';
+        return inputAmount;
+        
+    }
+    else{
+        document.getElementById(inputId + '-error').style.display='block';
+        document.getElementById(inputId + '-fillup').style.display='none';
+    }
+    
+    
+    
+    
+    
+    
+    
 }
 
 // calculate button and calculation 
@@ -15,6 +37,7 @@ document.getElementById('calculate-total').addEventListener('click',function(){
     // income amount call 
 
         const inputIncomeAmount = getInput('input-income');
+        
      
     // Expences input amount call 
         const inputFoodAmount =  getInput('food-input');
@@ -24,20 +47,46 @@ document.getElementById('calculate-total').addEventListener('click',function(){
         const inputClothAmount = getInput('cloth-input');
     
     // sum of total Expences
-        let totalExpencesSum = inputFoodAmount + inputRentAmount + inputClothAmount;
+        const totalExpencesSum = inputFoodAmount + inputRentAmount + inputClothAmount;
+       
         
     // total Expences
-    const totalExpences = document.getElementById('total-expenses');
-    const totalExpencesInnerText = totalExpences.innerText;
-    totalExpences.innerText = totalExpencesSum;
-    
+        const totalExpences = document.getElementById('total-expenses');
+        const totalExpencesInnerText = totalExpences.innerText;
+        // totalExpences.innerText = totalExpencesSum;
+   
     // total Balance
-    const inputBalance = document.getElementById('input-balance');
-    const inputBalanceInnerText = inputBalance.innerText;
-    inputBalance.innerText = inputIncomeAmount - totalExpencesSum;
+        const inputBalance = document.getElementById('input-balance');
+        const inputBalanceInnerText = inputBalance.innerText;
+    
 
     
+    if(isNaN(totalExpencesSum) == false){
+        totalExpences.innerText= totalExpencesSum;
+        inputBalance.innerText = inputIncomeAmount - totalExpencesSum;
+    }
+    else{
+        totalExpences.innerText= '00';
+        inputBalance.innerText = '00';
+    }
+
 });
+
+
+document.getElementById('clear-expenses').addEventListener('click',function(){
+
+    document.getElementById('food-input').value = '';
+    document.getElementById('rent-input').value = '';
+    document.getElementById('cloth-input').value = '';
+
+   
+
+})
+
+
+
+
+
 
 // save button added and calculation 
 
@@ -47,9 +96,11 @@ document.getElementById('calculate-saving').addEventListener('click',function(){
 
 
     // input field of savings Amount
-    const inputSave = document.getElementById('save-input');
-    const inputSaveValue = inputSave.value;
-    const inputSaveAmount = parseFloat(inputSaveValue);
+    const inputSaveAmount= getInput('save-input');
+
+    // const inputSave = document.getElementById('save-input');
+    // const inputSaveValue = inputSave.value;
+    // const inputSaveAmount = parseFloat(inputSaveValue);
     
     // savings Amount calculation 
     const savingCalculation = (inputIncomeAmount * inputSaveAmount) / 100;
@@ -58,7 +109,7 @@ document.getElementById('calculate-saving').addEventListener('click',function(){
     const savingAmount = document.getElementById('saving-amount');
     const savingAmountInnerText = savingAmount.innerText;
     const savingAmountParse = parseFloat(savingAmountInnerText);
-    savingAmount.innerText = savingCalculation;
+    // savingAmount.innerText = savingCalculation;
 
 
     // total balance calculation again to see the remainingBalance
@@ -74,9 +125,16 @@ document.getElementById('calculate-saving').addEventListener('click',function(){
     // declare remaining balance 
     const remainingBalance = document.getElementById('remaining-balance');
     const remainingBalanceInnerText = remainingBalance.innerText;
-    remainingBalance.innerText = remainingBalanceCalculation;
+    //  remainingBalance.innerText = remainingBalanceCalculation;
 
-    
+    if(isNaN(savingCalculation)==false ){
+        savingAmount.innerText = savingCalculation;
+        remainingBalance.innerText = remainingBalanceCalculation;
+    }
+    else{
+        savingAmount.innerText = '00';
+        remainingBalance.innerText = '00';
+    }
     
 });
 
