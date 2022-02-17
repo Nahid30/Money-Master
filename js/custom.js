1d2888dd741f2e1bad8c8de0f3d4de532285095f
Nahid30 <nahidshuvo30@gmail.com>
2022-02-17 08:46:45 +0600

    comment added everywhere

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 
+// function for calculate button and calculation 
 function getInput(inputId){
     const inputField = document.getElementById(inputId);
     const inputValue = inputField.value;
@@ -7,35 +8,30 @@ function getInput(inputId){
     return inputAmount;
 }
 
+// calculate button and calculation 
 
 document.getElementById('calculate-total').addEventListener('click',function(){
 
-   
-    
-
-    
-
+    // income amount call 
 
         const inputIncomeAmount = getInput('input-income');
      
-   
+    // Expences input amount call 
         const inputFoodAmount =  getInput('food-input');
-
 
         const inputRentAmount = getInput('rent-input');
 
-
         const inputClothAmount = getInput('cloth-input');
     
-    
+    // sum of total Expences
         let totalExpencesSum = inputFoodAmount + inputRentAmount + inputClothAmount;
         
-
+    // total Expences
     const totalExpences = document.getElementById('total-expenses');
     const totalExpencesInnerText = totalExpences.innerText;
     totalExpences.innerText = totalExpencesSum;
     
-
+    // total Balance
     const inputBalance = document.getElementById('input-balance');
     const inputBalanceInnerText = inputBalance.innerText;
     inputBalance.innerText = inputIncomeAmount - totalExpencesSum;
@@ -43,37 +39,39 @@ document.getElementById('calculate-total').addEventListener('click',function(){
     
 });
 
-
+// save button added and calculation 
 
 document.getElementById('calculate-saving').addEventListener('click',function(){
-
+    // call the function
     const inputIncomeAmount = getInput('input-income');
 
+
+    // input field of savings Amount
     const inputSave = document.getElementById('save-input');
     const inputSaveValue = inputSave.value;
     const inputSaveAmount = parseFloat(inputSaveValue);
     
-
+    // savings Amount calculation 
     const savingCalculation = (inputIncomeAmount * inputSaveAmount) / 100;
     
-
+    // declare savings ammount
     const savingAmount = document.getElementById('saving-amount');
     const savingAmountInnerText = savingAmount.innerText;
     const savingAmountParse = parseFloat(savingAmountInnerText);
     savingAmount.innerText = savingCalculation;
 
 
-
+    // total balance calculation again to see the remainingBalance
     const inputBalance = document.getElementById('input-balance');
     const inputBalanceInnerText = inputBalance.innerText;
     const inputBalanceAmountParse = parseFloat(inputBalanceInnerText);
 
     
-    
+    // remainingBalance calculation 
     const remainingBalanceCalculation = inputBalanceAmountParse - savingCalculation;
 
    
-
+    // declare remaining balance 
     const remainingBalance = document.getElementById('remaining-balance');
     const remainingBalanceInnerText = remainingBalance.innerText;
     remainingBalance.innerText = remainingBalanceCalculation;
